360c952b067297173976325af3d6e05573909896
Aksh4code <akkisingh8266@gmail.com>
2024-11-29 13:26:59 +0530

    .

--- event-logging-frontend/src/api/apiService.jsx
+++ event-logging-frontend/src/api/apiService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://event-logging.onrender.com/api/'; 
+const BASE_URL = 'https://event-logging.onrender.com/api'; 
 
 export const fetchLogs = async () => {
   try {
