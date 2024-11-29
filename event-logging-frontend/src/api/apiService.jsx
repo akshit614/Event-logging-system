399b1ee863fdc86d3741fb5670f426453aabf506
Aksh4code <akkisingh8266@gmail.com>
2024-11-29 13:23:56 +0530

    minor fix

--- event-logging-frontend/src/api/apiService.jsx
+++ event-logging-frontend/src/api/apiService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api'; 
+const BASE_URL = 'https://event-logging.onrender.com/api/'; 
 
 export const fetchLogs = async () => {
   try {
