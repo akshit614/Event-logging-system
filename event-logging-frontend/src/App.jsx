5b4a6e71036a50145fe7ef12cae451654580002a
Aksh4code <akkisingh8266@gmail.com>
2024-11-29 13:38:32 +0530

    added routing

--- event-logging-frontend/src/App.jsx
+++ event-logging-frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import Dashboard from './pages/Dashboard'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <Dashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
